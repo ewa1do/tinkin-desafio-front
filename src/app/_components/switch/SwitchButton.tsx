c96a67e4e89cdee81adef43836552a0c2d545957
Eduardo Vera <this.eduardovera@gmail.com>
2025-03-29 18:50:31 -0400

    fixing bug that reloads page when button is clicked

--- src/app/_components/switch/SwitchButton.tsx
+++ src/app/_components/switch/SwitchButton.tsx
@@ -2,13 +2,19 @@ import Image from "next/image";
 
 export function SwitchButton({
     isCookedBefore,
-    onclick,
+    onclick: onClick,
 }: {
     isCookedBefore: boolean;
     onclick: () => void;
 }) {
     return (
-        <button className="cursor-pointer" onClick={onclick}>
+        <button
+            className="cursor-pointer"
+            onClick={(e) => {
+                e.preventDefault();
+                onClick();
+            }}
+        >
             <Image
                 src={`/${isCookedBefore ? "switch-active.svg" : "switch-inactive.svg"}`}
                 alt="switch-button"
