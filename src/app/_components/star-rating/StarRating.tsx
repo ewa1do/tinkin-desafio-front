bb70c440ef035bbe408b720175093bfbba631c98
Eduardo Vera <this.eduardovera@gmail.com>
2025-03-29 18:28:28 -0400

    updating StarRating component to handle sizes

--- src/app/_components/star-rating/StarRating.tsx
+++ src/app/_components/star-rating/StarRating.tsx
@@ -2,25 +2,33 @@ import Image from "next/image";
 
 export const MAX_STARS_ALLOWED = 4;
 
-export function StarRating({ reviews }: { reviews: number }) {
+export function StarRating({ reviews, size = "md" }: { reviews: number; size: "md" | "lg" }) {
     if (reviews > MAX_STARS_ALLOWED) reviews = MAX_STARS_ALLOWED;
 
     return (
         <div>
             <i className="flex" data-testid="images">
-                <PlaceStars reviews={reviews} active={true} />
-                <PlaceStars reviews={MAX_STARS_ALLOWED - reviews} active={false} />
+                <PlaceStars reviews={reviews} active={true} size={size} />
+                <PlaceStars reviews={MAX_STARS_ALLOWED - reviews} active={false} size={size} />
             </i>
         </div>
     );
 }
 
-function PlaceStars({ reviews, active }: { reviews: number; active: boolean }) {
+function PlaceStars({
+    reviews,
+    active,
+    size = "md",
+}: {
+    reviews: number;
+    active: boolean;
+    size: "md" | "lg";
+}) {
     return Array.from({ length: reviews }).map((_, idx) => (
         <Image
             src={`${active ? "rating-star.svg" : "rating-star-inactive.svg"}`}
             alt="star"
-            width={16}
+            width={`${size === "md" ? 16 : 20}`}
             height={16}
             key={`--star--${idx}`}
         />
